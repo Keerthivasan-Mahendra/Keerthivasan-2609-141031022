c7246675419ddb5349eaf167783002f49ba59700
keerthivasan.k <keerthivasan.k@nutanix.com>
2023-03-16 12:26:56 +0530

    add reqs

--- api/trade.py
+++ api/trade.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
 import gdown
+
 app = Flask(__name__)
+
 
 @app.route('/run-colab')
 def run_colab():
     gdown.download('https://drive.google.com/file/d/1m18XfKKVRi-V9id3JpC1u6FbEUKmecjk#scrollTo=pUYnw76WSQyk', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run()
